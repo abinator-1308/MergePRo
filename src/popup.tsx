ea01f13ce49bc077637607d586ba9c71558cca8d
IN22914350 <arjch0302@gmail.com>
2020-09-12 08:36:01 +0530

    Setup : Adding webpack and typescript

---
+++ src/popup.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h3>HELLO TO MERGE PRO</h3>
+    <p>VIEW AND MANAGE ALL YOUR PRs AT ONE PLACE AND BE PRO!!!!</p>
+  </div>,
+  document.getElementById("root")
+);
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require("path");
+const webpack = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        include: [path.resolve(__dirname, "src")],
+        loader: "babel-loader",
+        test: /\.js$/,
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+        ],
+      },
+    ],
+  },
+  entry: {
+    popup: "./src/popup.tsx",
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".json"],
+  },
+  output: {
+    filename: "[name].js",
+    publicPath: "/",
+  },
+  mode: "development",
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    compress: true,
+    watchContentBase: true,
+    hot: true,
+    port: 9000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "popup.html"),
+      inject: true,
+      chunks: ["popup"],
+    }),
+    new webpack.IgnorePlugin(/^\.\/locale$/),
+    ...(process.env.BUNDLE_ANALYZER ? [new BundleAnalyzerPlugin()] : []),
+  ],
+};
