498a7e04197102fad310bfc9f88a1716f0afe764
IN22914350 <arjch0302@gmail.com>
2020-09-12 13:18:00 +0530

    Add mobx state and chrome messenger api

--- src/environment/implementation.ts
+++ src/environment/implementation.ts
@@ -1,13 +1,17 @@
 import { ChromeApi } from "../chrome/api";
 import { buildGitHubLoader } from "../loading/implementation";
+import { buildMessenger } from "../messaging/implementation";
 import { buildStore } from "../storage/implementation";
 import { Environment } from "./api";
 
 export function buildEnvironment(chromeApi: ChromeApi): Environment {
   const store = buildStore(chromeApi);
+  const getCurrentTime = () => Date.now();
 
   return {
     store,
     githubLoader: buildGitHubLoader(),
+    messenger: buildMessenger(chromeApi),
+    getCurrentTime,
   };
 }
