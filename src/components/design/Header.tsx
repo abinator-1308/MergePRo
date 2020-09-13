7ba1ee39b5619168d80161d8566af1e4cef3ebc5
IN22914350 <arjch0302@gmail.com>
2020-09-13 13:52:04 +0530

    Added updates

--- src/components/design/Header.tsx
+++ src/components/design/Header.tsx
@@ -2,6 +2,6 @@ import styled from "@emotion/styled";
 
 export const Header = styled.h1`
   font-size: 14px;
-  text-align: left;
-  border-radius: 100px;
+  margin-left: 6px;
+  font-weight: 600;
 `;
